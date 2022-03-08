77de11950a7f9c57ab0ccf3b24b14c4ec79d2602
Emmanuel-R8 <emmanuel.rialland@gmail.com>
2022-03-08 14:48:08 +0800

    Tullio improvements

--- scratchpad.jl
+++ scratchpad.jl
@@ -1,9 +1,19 @@
-function (a::AdaptiveMaxPool{S})(x::AbstractArray{T, S}) where {S, T}
-  insize = size(x)[1:end-2]
-  outsize = a.out
-  stride = insize .÷ outsize
-  k = insize .- (outsize .- 1) .* stride
-  pad = 0
-  pdims = PoolDims(x, k; padding=pad, stride=stride)
-  return maxpool(x, pdims)
-end
+pwd()
+using Pkg;
+Pkg.activate(".");
+
+using SharpenedCorrelation, Debugger
+
+break_on(:error)
+Debugger.@run SharpenedConvolution(28, 28, 1, 25, 5, 2, 1)
+
+SharpenedConvolution(28, 28, 1, 25, 5, 2, 1)
+
+patch_convolution(::Array{Float32,4},
+                  ::SubArray{Float32,3,
+                             PaddedView{Float32,4,NTuple{4,UnitRange{Int64}},
+                                        OffsetArrays.OffsetArray{Float32,4,
+                                                                 Array{Float32,4}}},
+                             Tuple{UnitRange{Int64},UnitRange{Int64},
+                                   Base.Slice{UnitRange{Int64}},Int64},false}, ::Int64,
+                  ::Float32, ::Float32, ::Int64)
